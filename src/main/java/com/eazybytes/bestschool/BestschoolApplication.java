b4bc16506530770edf86884b125d5e6c42619d2e
An5H <dhimanansh7@gmail.com>
2022-08-16 15:41:19 +0530

    Home page completed

---
+++ src/main/java/com/eazybytes/bestschool/BestschoolApplication.java
@@ -0,0 +1,13 @@
+package com.eazybytes.bestschool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BestschoolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BestschoolApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/eazybytes/bestschool/BestschoolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eazybytes.bestschool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BestschoolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
