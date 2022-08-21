ac77aff234acd5fb050f37d24978854046272816
An5H <dhimanansh7@gmail.com>
2022-08-21 14:26:58 +0530

    Added lombok library

--- src/main/java/com/eazybytes/bestschool/model/Holiday.java
+++ src/main/java/com/eazybytes/bestschool/model/Holiday.java
@@ -1,9 +1,14 @@
 package com.eazybytes.bestschool.model;
 
+import lombok.Data;
+
 // classes ehich are essentially data carriers containing only constructors and getter methods are converted into records
-public record Holiday(String day, String reason, com.eazybytes.bestschool.model.Holiday.Type type) {
+@Data
+public final class Holiday {
+    private final String day;
+    private final String reason;
+    private final Type type;
     public enum Type {
         FESTIVAL, FEDERAL
     }
-
 }
--- src/main/java/com/eazybytes/bestschool/service/ContactService.java
+++ src/main/java/com/eazybytes/bestschool/service/ContactService.java
@@ -1,14 +1,12 @@
 package com.eazybytes.bestschool.service;
 
 import com.eazybytes.bestschool.model.Contact;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
-import java.util.logging.Logger;
-
+@Slf4j
 @Service
 public class ContactService {
-    private final Logger log = Logger.getLogger(ContactService.class.toString());
-
     public void saveMessageDetails(Contact contact){
         log.info(contact.toString());
     }
