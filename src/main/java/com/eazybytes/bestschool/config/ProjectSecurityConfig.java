a39de0da207bb64afc7ccf995d4eaf4a467e8fc9
An5H <dhimanansh7@gmail.com>
2022-08-29 19:31:06 +0530

    Added dashboard, login page with login/logout functionality

--- src/main/java/com/eazybytes/bestschool/config/ProjectSecurityConfig.java
+++ src/main/java/com/eazybytes/bestschool/config/ProjectSecurityConfig.java
@@ -1,15 +1,37 @@
 package com.eazybytes.bestschool.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 
 @Configuration
 public class ProjectSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests().anyRequest().permitAll()
-                .and().formLogin()
+
+        http.authorizeRequests()
+                .mvcMatchers("/dashboard").authenticated()
+                .mvcMatchers("/home").permitAll()
+                .mvcMatchers("/holidays/**").permitAll()
+                .mvcMatchers("/contact").permitAll()
+                .mvcMatchers("/saveMsg").permitAll()
+                .mvcMatchers("/courses").permitAll()
+                .mvcMatchers("/about").permitAll()
+                .mvcMatchers("/login").permitAll()
+                .and().formLogin().loginPage("/login")
+                .defaultSuccessUrl("/dashboard")
+                .failureUrl("/login?error=true").permitAll()
+                .and().logout().logoutSuccessUrl("/login?logout=true").invalidateHttpSession(true).permitAll()
                 .and().httpBasic();
+    }
+
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication().
+                withUser("ansh").password("ansh").roles("USER")
+                .and()
+                .withUser("admin").password("admin").roles("USER", "ADMIN")
+                .and().passwordEncoder(NoOpPasswordEncoder.getInstance());
     }
 }
